2e74bca8461f192a436b53d93320d0e78d8a706e
Nirdesh <1onlynirdesh@gmail.com>
2023-11-28 02:44:15 +0530

    Port Commit

--- app.py
+++ app.py
@@ -66,5 +66,8 @@ def upload_file():
         }
         return render_template('result.html', result=result)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=7000)
